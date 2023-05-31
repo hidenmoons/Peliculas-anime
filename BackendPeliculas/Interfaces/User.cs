ecfd1cf0db30420fed5bb93ad43b8d01a29821d9
Joan Luna <alexis_dx8@hotmail.com>
2023-05-30 23:55:05 -0700

    creando interface

--- BackendPeliculas/Interfaces/User.cs
+++ BackendPeliculas/Interfaces/User.cs
@@ -2,5 +2,11 @@
 {
     public interface User
     {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+             
     }
 }
