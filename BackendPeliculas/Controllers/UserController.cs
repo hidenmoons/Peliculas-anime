e81943ad93645f9076efe3ed45c3f79ae9d1f558
Joan Luna <alexis_dx8@hotmail.com>
2023-05-30 23:51:34 -0700

    creando endpoint de registor de usuario con asp indentity

---
+++ BackendPeliculas/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BackendPeliculas.Models;
+using BackendPeliculas.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+namespace BackendPeliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> userManager;
+        
+        public UserController(UserManager<User> userManager) 
+        {
+            this.userManager= userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create_user(UserManager<User> userManager) 
+        {
+            var newUser = new User
+            {
+                Email = "test@demo.com",
+                FirstName = "Test",
+                LastName = "User",
+                UserName = "test.demo"
+            };
+            var result = await userManager.CreateAsync(newUser,"12345678");
+            return Ok(result);
+        }
+    }
+}
